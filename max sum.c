c80213ae53b13c2fcc7eea25c9cec3e8e36daa23
prathi8081 <58175552+prathi8081@users.noreply.github.com>
2020-05-01 18:29:08 +0530

    Add files via upload

---
+++ max sum.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+
+int main()
+{
+  
+ int n,a[10],i,first,second;
+  
+ scanf("%d",&n);
+ 
+  for(i=0;i<n;i++)
+   
+{
+       scanf("%d",&a[i]);
+ 
+  }
+  
+ if(a[0]>a[1])
+
+   {
+  
+   first=a[0];
+   
+  second=a[1];
+  
+ }
+   
+else
+   {
+ 
+ 
+
+  first=a[1];
+  
+  second=a[0];
+  
+ }
+  
+ for(i=2;i<n;i++)
+ 
+  {
+      
+ if(a[i]>first)
+   
+    {
+       
+ second=first;
+   
+     first=a[i];
+    
+   }
+     
+  if(a[i]>second && a[i]!=first)
+ 
+      {
+        second=a[i];
+     
+  }
+  
+ }
+  
+ printf("%d ",first+second);
+
+}
---
+++ sum of diag in matrix.c
@@ -0,0 +1,38 @@
+#include <stdio.h> 
+
+
+int main()
+
+{
+  
+int n,c,a[10][10],j,i,d1=0,d2=0;
+
+  scanf("%d %d",&n,&c);
+ 
+ for(i=0;i<n;i++)
+
+  {
+     
+   for(j=0;j<c;j++)
+   
+         scanf("%d",&a[i][j]);
+
+  }
+  
+for(i=0;i<n;i++)
+  
+
+{
+    
+  d1+=a[i][i];
+
+      d2+=a[i][n-i-1];
+ 
+ }
+
+           
+     printf("%d",d1+d2-a[n/2][n/2]);
+  
+
+    
+}
