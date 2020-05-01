4f72bc7756f8edafe469bca7bad9f817613c9cc6
prathi8081 <58175552+prathi8081@users.noreply.github.com>
2020-05-01 18:27:03 +0530

    Add files via upload

---
+++ words to numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+ 
+{
+ 
+
+char s[20] ;
+  
+ scanf("%s",s);
+  
+ int score=0;
+  
+ int n = strlen(s);
+ 
+
+  for (int j = 0; j < n; j++) 
+
+        { 
+           
+ score += s[j] - 'a' + 1;
+  
+      } 
+   
+
+ printf("%d",score);
+
+}
